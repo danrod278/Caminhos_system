a4eb1328c855d510a939d478d6781bbc37460541
Daniel Rodrigues <danrod278@gmail.com>
2023-11-29 21:18:34 -0300

    login ok

---
+++ funcoes.js
@@ -0,0 +1,33 @@
+const {MongoClient} = require("mongodb")
+
+
+const url_database = 'mongodb://localhost:27017/teste05' 
+const client = new MongoClient(url_database,{useNewUrlParser: true, useUnifiedTopology: true })
+
+async function verificador_login(collection, login){
+    try{
+        await client.connect()
+        const db = client.db()
+        const resposta = await (db.collection(collection)).find().toArray()
+       
+        if (resposta.length>0){
+            for(let contador=0;contador<=resposta.length;contador++){
+                if(contador==resposta.length){
+                    return false
+                }
+                let analista = resposta[contador]
+                if(analista.username==login.username && analista.password==login.senha){
+                    const retorno =[true, ''+analista._id+'']
+                    return retorno   
+                }
+                
+                
+            }
+        }
+    }finally{
+        await client.close()
+       
+    }
+}
+module.exports = {verificador_login}
+
--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const body_parser = require("body-parser"); 
+const bodyParser = require("body-parser");
+const funcoes = require('./funcoes.js');
+const { stringify } = require("querystring");
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static((path.join(__dirname,'public'))))
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/', (req, res)=>{
-    res.send("Está funcionando!")
+    res.render("login")
 })
-app.get('/sobre', (req, res)=>{
-    res.render("sobre")
+app.post('/conferir', (req, res)=>{
+
+    let dados_form=req.body
+    async function acesso(){
+        const acess = await funcoes.verificador_login("nomecolecao", dados_form)
+        console.log(acess)
+        if(acess[0]){
+            res.redirect(`/inicial/:${acess[1]}`)
+        }else{
+            res.redirect('/')
+        }
+    }
+    acesso()
+     
+})
+app.get('/inicial/:id',(req, res)=>{
+    res.send('ok')
 })
 
-app.listen(2007, "192.168.15.5", ()=>{
-    console.log("Funcionando")
+
+
+const port = 202
+app.listen(port, "192.168.15.11", ()=>{
+    console.log(`Funcionando; Porta: http://192.168.15.11:${port}`)
 })
+
